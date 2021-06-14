8c1f00bc726ff48c0904f13000994523a61370c0
mixelio <mixelios@gmail.com>
2021-06-14 13:02:50 +0300

    lk

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".testimonials__slider").slick({
+    arrows: true,
+    slidesToShow: 3,
+    infinite: true,
+    speed: 500,
+    //autoplay: true,
+    autoplaySpeed: 6000,
+  });
+});
