bea2f95148e9cac9afa31633efd56f342bf75ad2
mixelio <mixelios@gmail.com>
2021-06-15 15:43:32 +0300

    ad

--- js/script.js
+++ js/script.js
@@ -1,12 +1,59 @@
 "use strict";
 
 $(document).ready(function () {
+  const mediaQuery = window.matchMedia("(max-width: 1009px)");
+  if (mediaQuery.matches) {
+    $(".testimonials__slider").slick({
+      arrows: true,
+      slidesToShow: 2,
+      infinite: true,
+      speed: 500,
+      autoplaySpeed: 6000,
+    });
+  }
   $(".testimonials__slider").slick({
     arrows: true,
     slidesToShow: 3,
     infinite: true,
     speed: 500,
-    //autoplay: true,
     autoplaySpeed: 6000,
   });
 });
+
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
